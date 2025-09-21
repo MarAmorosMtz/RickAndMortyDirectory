a68938613d345ef94c551881a1390fac04b440c2
Papimar <deadzoneoficial666@gmail.com>
2025-09-21 14:01:48 -0600

    Character pages requested to Rick&Morty API, loaded and shown. Paginated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
-import { Component, signal } from '@angular/core';
+import { Component, resource, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, NgxPaginationModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
+  standalone: true
 })
 export class App {
-  protected readonly title = signal('RickAndMortyDirectory');
+  p = signal(1);
+  paginationId = 'charactersPagination';
+
+  results = resource({
+    params: () => ({page: this.p()}),
+    loader: async ({params}) => {
+      const res = await fetch(`https://rickandmortyapi.com/api/character?page=${params.page}`);
+      return res.json();
+    }
+  });
+
+  nextPage() {
+    const next = this.results.value()?.info.next;
+    if (next) {
+      this.p.set(this.p() + 1);
+    }
+  }
+
+  prevPage() {
+    if (this.p() > 1) {
+      this.p.set(this.p() - 1);
+    }
+  }
+
+  trackById(index: number, item: any) {
+    return item.id;
+  }
 }
