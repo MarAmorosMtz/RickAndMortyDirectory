4b17dd6d0d7092017e0926377cbb1243c1aac5f2
Papimar <deadzoneoficial666@gmail.com>
2025-09-21 16:58:08 -0600

    Filter Seccion added. Connection to Rick&MortyApi's Episode endpoint to track and filter by seasons

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgxPaginationModule } from 'ngx-pagination';
 
+interface Character {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: { name: string };
+  location: { name: string };
+  episode: string[];
+  image: string;
+}
+
+interface Episode {
+  id: number;
+  name: string;
+  episode: string;
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, CommonModule, NgxPaginationModule],
@@ -13,6 +32,28 @@ import { NgxPaginationModule } from 'ngx-pagination';
 export class App {
   p = signal(1);
   paginationId = 'charactersPagination';
+  filterCategories = [
+    { label: 'All', value: 'all' },
+    { label: 'Alive', value: 'alive' },
+    { label: 'Dead', value: 'dead' },
+    { label: 'Male', value: 'male' },
+    { label: 'Female', value: 'female' }
+  ];
+  selectedCategory = signal('');
+
+  seasons = [
+    'Season 1', 'Season 2', 'Season 3', 'Season 4', 
+    'Season 5', 'Season 6'
+  ];
+
+  selectedSeason = signal('');
+
+
+  search = signal('');
+  showFilters = signal(false);
+  selectedFilter = signal('');
+  filterValue = signal('');
+  episodesBySeason: Record<string, Episode[]> = {};
 
   results = resource({
     params: () => ({page: this.p()}),
@@ -21,6 +62,30 @@ export class App {
       return res.json();
     }
   });
+
+  constructor() {
+    this.loadEpisodes();
+  }
+
+  async loadEpisodes() {
+    let page = 1;
+    let episodes: Episode[] = [];
+    let totalPages = 1;
+
+    do {
+      const res = await fetch(`https://rickandmortyapi.com/api/episode?page=${page}`);
+      const data = await res.json();
+      episodes.push(...data.results);
+      totalPages = data.info.pages;
+      page++;
+    } while (page <= totalPages);
+
+    episodes.forEach(ep => {
+      const season = `Season ${parseInt(ep.episode.slice(1, 3))}`; // "S01E01" -> "Season 1"
+      if (!this.episodesBySeason[season]) this.episodesBySeason[season] = [];
+      this.episodesBySeason[season].push(ep);
+    });
+  }
 
   nextPage() {
     const next = this.results.value()?.info.next;
@@ -35,7 +100,46 @@ export class App {
     }
   }
 
+  firstPage(){
+    this.p.set(1)
+  }
+
   trackById(index: number, item: any) {
     return item.id;
+  }
+
+  get filteredResults(): Character[] {
+  let list: Character[] = this.results.value()?.results || [];
+
+  if (this.search()) {
+    const filter = this.search().toLowerCase();
+    list = list.filter(c => c.name.toLowerCase().includes(filter));
+  }
+
+  const cat = this.selectedCategory();
+  if (cat) {
+    list = list.filter(c => {
+      switch (cat) {
+        case 'all': return true;
+        case 'alive': return c.status && c.status == 'Alive';
+        case 'dead': return c.status && c.status == 'Dead';
+        case 'male': return c.gender && c.gender == 'Male';
+        case 'female': return c.gender && c.gender == 'Female';
+        default: return true;
+      }
+    });
+  }
+
+  const season = this.selectedSeason();
+    if (season && this.episodesBySeason[season]) {
+      const episodeUrls = this.episodesBySeason[season].map(e => `https://rickandmortyapi.com/api/episode/${e.id}`);
+      list = list.filter(c => c.episode.some(ep => episodeUrls.includes(ep)));
+    }
+
+    return list;
+  }
+
+  setSeason(season: string) {
+    this.selectedSeason.set(season);
   }
 }
